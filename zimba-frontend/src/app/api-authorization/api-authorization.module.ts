91ff95106c8ddeb0a990b9e8dd863194bf855f52
Suleman Khan <sulemankk010@gmail.com>
2022-11-04 18:17:20 +0500

    theme setup and login page design

--- zimba-frontend/src/app/api-authorization/api-authorization.module.ts
+++ zimba-frontend/src/app/api-authorization/api-authorization.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  // {
+  //   path: 'signup',
+  //   component: SignupComponent
+  // },
+  {
+    path: '**',
+    redirectTo: 'login'
+  }
+];
 
 
 @NgModule({
@@ -9,7 +24,8 @@ import { LoginComponent } from './login/login.component';
     LoginComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class ApiAuthorizationModule { }
--- zimba-frontend/src/app/app-routing.module.ts
+++ zimba-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'authentication',
+  loadChildren: () =>
+    import('./api-authorization/api-authorization.module').then(
+      (m) => m.ApiAuthorizationModule
+    )
+},
+{
+  path: '**',
+  redirectTo: 'authentication',
+  pathMatch: 'full'
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
